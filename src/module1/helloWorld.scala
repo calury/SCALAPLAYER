963d3db4c4b6606fb7c7eda788ac5f9eb9119f8d
calury <zhchengzhi2007@163.com>
2021-01-16 15:48:37 +0800

    test2

---
+++ src/module1/helloWorld.scala
@@ -0,0 +1,10 @@
+package module1
+
+class helloWorld {
+  def main(args: Array[String]): Unit = {
+    val testString = "zhangchengzhi"
+    println("Hello World")
+    println(testString)
+  }
+
+}
